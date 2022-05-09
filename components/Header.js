f5357bbb2e74af9587323f596457a4732179c0f7
ins4w <amejia.foo@gmail.com>
2022-05-08 23:42:31 -0500

    fix: nav classes

--- components/Header.js
+++ components/Header.js
@@ -2,17 +2,17 @@ const template = document.createElement("template");
 
 template.innerHTML = `
   <nav>
-    <ul class="nav-links">
-      <li>
+    <ul class="list-links nav-links">
+      <li class="list-link margin-list-link">
         <a href="./index.html">Home</a>
       </li>
-      <li>
+      <li class="list-link margin-list-link">
         <a href="./projects.html">Projects</a>
       </li>
-      <li>
+      <li class="list-link margin-list-link">
         <a href="./events.html">Events</a>
       </li>
-      <li>
+      <li class="list-link">
         <a href="./contact.html">Contact</a>
       </li>
     </ul>
